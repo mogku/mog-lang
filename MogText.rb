7c972d688f8de9b1b10234c574c5d65c8e3470f5
mogku <alisha.taflove@gmail.com>
2025-02-27 16:33:59 -0800

    Update MogText.rb

--- MogText.rb
+++ MogText.rb
@@ -12,33 +12,3 @@ end
 
 
 
-
-#For later: class Ring
-    #def Ring.summon -> {}
-       # self.summon
-        
-   # end
-    
-#end
-
-
-
-=begin class Text 
-    def text.self(text)
-        puts "
-
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis sapien lobortis, tincidunt odio non, posuere risus. Integer commodo neque posuere, commodo ex vitae, congue dui. Donec mauris nisl, pharetra id lacus eu, pharetra varius velit. Etiam in sem eu elit suscipit aliquam vitae vel tellus. Donec eget finibus est. Aenean nec posuere lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-"
-end =end
-
-
-
-
-
-
-
-
-#Text.summon mog.summon (Takes arguments) Module.mog (Module) Module.class Mog.manager [to.string] text.print file.load file.load(img, time,) file.givePermissions() mog++*number to go up to* loopMog Mogdata(Mogata.X, ) Mog.addShape(X, Y, Z, mogpos) mog.X, mog.Y, mog.Z, mog.addClass mog.math(input math) mog.findSum mog.Grid mog.Manager.gridMesh grid.Index.manager
-#Grid indexing crossshades everything with squares that have ID's each one and you can class them add them and manipulate they're meshes. Including: Mesh colors and more
